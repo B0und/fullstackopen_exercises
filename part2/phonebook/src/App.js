3fb52b784e6b9bb54ecd71a7738f7b321e40819f
b0und <f1arenova1@gmail.com>
2021-07-06 21:52:16 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const NumbersDisplay = ({ persons }) => {
+  // console.log(persons);
   return (
     <div>
       {persons.map((person) => (
@@ -13,9 +14,20 @@ const NumbersDisplay = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "8-800-555-35-35" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [showFiltered, setShowFiltered] = useState(true);
+  const [newFilter, setNewFilter] = useState("")
+
+  const filteredPhones = showFiltered 
+  ? persons 
+  : persons.filter(person => person.name.toLowerCase().startsWith(newFilter.toLowerCase()))
 
   const addNumber = (event) => {
     event.preventDefault();
@@ -33,7 +45,7 @@ const App = () => {
 
     const newPerson = {
       name: newName,
-      number : newNumber
+      number: newNumber,
     };
     setPersons(persons.concat(newPerson));
     setNewName("");
@@ -47,9 +59,23 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const onFilterChange = (event) => {
+    setNewFilter(event.target.value);
+    if (newFilter.length !== 0) {
+      setShowFiltered(true)
+    }
+    else {
+      setShowFiltered(false)
+    }
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search names: <input onChange={onFilterChange} />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addNumber}>
         <div>
           name: <input onChange={onNameChange} />
@@ -60,7 +86,7 @@ const App = () => {
         <button type="submit">add</button>
       </form>
       <h2>Numbers</h2>
-      <NumbersDisplay persons={persons} />
+      <NumbersDisplay persons={filteredPhones} />
     </div>
   );
 };
